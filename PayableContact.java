f605d289fb8ae7d11062245b12e1f3445990a1d7
Alexander Wang <48783729+alexwangraymond@users.noreply.github.com>
2019-06-29 01:18:12 -0400

    Add files via upload
    these 6 files are part of the contacts package, which is used in the class file "CompanyMaker"

---
+++ PayableContact.java
@@ -0,0 +1,67 @@
+/**
+ * to be used as a temporary data holder during the creation
+ * of a new company. when user selects "new contact", the previous
+ * information will be stored in an object of this type, which will
+ * then be stored in an arraylist of such objects
+ */
+
+package contacts;
+
+public class PayableContact {
+	String name, cell, phone, fax, email;
+	
+	// constructor: creates an instance of this class
+	public PayableContact() {
+		
+	}
+	
+	// method: stores a name
+	public void storeName(String name) {
+		this.name = name;
+	}
+	
+	// method: stores a cell number
+	public void storeCell(String cellNumber) {
+		this.cell = cellNumber;
+	}
+	
+	// method: stores a phone number
+	public void storePhone(String phoneNumber) {
+		this.phone = phoneNumber;
+	}
+	
+	// method: stores a fax number
+	public void storeFax(String faxNumber) {
+		this.fax = faxNumber;
+	}
+	
+	// method: stores an email address
+	public void storeEmail(String emailAddress) {
+		this.email = emailAddress;
+	}
+	
+	// method: returns stored name
+	public String getName() {
+		return this.name;
+	}
+	
+	// method: returns a stored cell number
+	public String getCell() {
+		return this.cell;
+	}
+	
+	// method: returns a stored phone number
+	public String getPhone() {
+		return this.phone;
+	}
+	
+	// method: returns a stored fax number
+	public String getFax() {
+		return this.fax;
+	}
+	
+	// method: returns a stored email address
+	public String getEmail() {
+		return this.email;
+	}
+}
