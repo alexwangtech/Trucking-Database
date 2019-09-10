e37d0445045be4b8df38ee31d79001b2b0f6a3e4
Alexander Wang <48783729+alexander-chang-wang@users.noreply.github.com>
2019-09-10 13:00:20 -0400

    Add files via upload

---
+++ storageDevices/InsurerCompany.java
@@ -0,0 +1,137 @@
+package storageDevices;
+
+public class InsurerCompany
+{
+	// private members
+	private String insurer;
+	private String general_policy;
+	private String general_coverage;
+	private String general_expiration;
+	private String auto_policy;
+	private String auto_coverage;
+	private String auto_expiration;
+	private String cargo_policy;
+	private String cargo_coverage;
+	private String cargo_expiration;
+	private String targetGoodsDeductibles;
+	
+	// Constructor
+	public InsurerCompany()
+	{
+		
+	}
+	
+	// ===================
+	// GETTERS AND SETTERS
+	// ===================
+	
+	public String getInsurer()
+	{
+		return insurer;
+	}
+
+	public void setInsurer(String insurer)
+	{
+		this.insurer = insurer;
+	}
+
+	public String getGeneral_policy()
+	{
+		return general_policy;
+	}
+
+	public void setGeneral_policy(String general_policy)
+	{
+		this.general_policy = general_policy;
+	}
+
+	public String getGeneral_coverage()
+	{
+		return general_coverage;
+	}
+
+	public void setGeneral_coverage(String general_coverage)
+	{
+		this.general_coverage = general_coverage;
+	}
+
+	public String getGeneral_expiration()
+	{
+		return general_expiration;
+	}
+
+	public void setGeneral_expiration(String general_expiration)
+	{
+		this.general_expiration = general_expiration;
+	}
+
+	public String getAuto_policy()
+	{
+		return auto_policy;
+	}
+
+	public void setAuto_policy(String auto_policy)
+	{
+		this.auto_policy = auto_policy;
+	}
+
+	public String getAuto_coverage()
+	{
+		return auto_coverage;
+	}
+
+	public void setAuto_coverage(String auto_coverage)
+	{
+		this.auto_coverage = auto_coverage;
+	}
+
+	public String getAuto_expiration()
+	{
+		return auto_expiration;
+	}
+
+	public void setAuto_expiration(String auto_expiration)
+	{
+		this.auto_expiration = auto_expiration;
+	}
+
+	public String getCargo_policy()
+	{
+		return cargo_policy;
+	}
+
+	public void setCargo_policy(String cargo_policy)
+	{
+		this.cargo_policy = cargo_policy;
+	}
+
+	public String getCargo_coverage()
+	{
+		return cargo_coverage;
+	}
+
+	public void setCargo_coverage(String cargo_coverage)
+	{
+		this.cargo_coverage = cargo_coverage;
+	}
+
+	public String getCargo_expiration()
+	{
+		return cargo_expiration;
+	}
+
+	public void setCargo_expiration(String cargo_expiration)
+	{
+		this.cargo_expiration = cargo_expiration;
+	}
+
+	public String getTargetGoodsDeductibles()
+	{
+		return targetGoodsDeductibles;
+	}
+
+	public void setTargetGoodsDeductibles(String targetGoodsDeductibles)
+	{
+		this.targetGoodsDeductibles = targetGoodsDeductibles;
+	}
+}
---
+++ storageDevices/NLBotPanelDataHolder.java
@@ -0,0 +1,132 @@
+package storageDevices;
+
+public class NLBotPanelDataHolder
+{
+	String date;
+	String time_from;
+	String time_to;
+	String appt;
+	String company;
+	String address1;
+	String address2;
+	String city;
+	String state;
+	String zip;
+	String contact;
+	String tel;
+	String ref;
+	String note;
+	
+	public String getDate()
+	{
+		return date;
+	}
+	public void setDate(String date)
+	{
+		this.date = date;
+	}
+	public String getTime_from()
+	{
+		return time_from;
+	}
+	public void setTime_from(String time_from)
+	{
+		this.time_from = time_from;
+	}
+	public String getTime_to()
+	{
+		return time_to;
+	}
+	public void setTime_to(String time_to)
+	{
+		this.time_to = time_to;
+	}
+	public String getAppt()
+	{
+		return appt;
+	}
+	public void setAppt(String appt)
+	{
+		this.appt = appt;
+	}
+	public String getCompany()
+	{
+		return company;
+	}
+	public void setCompany(String company)
+	{
+		this.company = company;
+	}
+	public String getAddress1()
+	{
+		return address1;
+	}
+	public void setAddress1(String address1)
+	{
+		this.address1 = address1;
+	}
+	public String getAddress2()
+	{
+		return address2;
+	}
+	public void setAddress2(String address2)
+	{
+		this.address2 = address2;
+	}
+	public String getCity()
+	{
+		return city;
+	}
+	public void setCity(String city)
+	{
+		this.city = city;
+	}
+	public String getState()
+	{
+		return state;
+	}
+	public void setState(String state)
+	{
+		this.state = state;
+	}
+	public String getZip()
+	{
+		return zip;
+	}
+	public void setZip(String zip)
+	{
+		this.zip = zip;
+	}
+	public String getContact()
+	{
+		return contact;
+	}
+	public void setContact(String contact)
+	{
+		this.contact = contact;
+	}
+	public String getTel()
+	{
+		return tel;
+	}
+	public void setTel(String tel)
+	{
+		this.tel = tel;
+	}
+	public String getRef()
+	{
+		return ref;
+	}
+	public void setRef(String ref)
+	{
+		this.ref = ref;
+	}
+	public String getNote()
+	{
+		return note;
+	}
+	public void setNote(String note)
+	{
+		this.note = note;
+	}
+}
